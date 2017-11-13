6971fcadd59d1e07bfe69259eeebc9ec2f659d25
yiakwy <yiak.wy@gmail.com>
2017-11-13 16:17:36 +0800

    update ConvNet

--- Correlation Metrics/ConvNet/costImpl/ConvNet/layers.py
+++ Correlation Metrics/ConvNet/costImpl/ConvNet/layers.py
@@ -2,6 +2,7 @@ __author__ = 'wangyi'
 # Created on 1st Nov 2017
 
 from ..common.node import Node
+from .vol import Vol
 import math
 import numpy as np
 
@@ -19,14 +20,54 @@ class Layer(Node):
         name = None
         super(Layer, self).__init__(name, data)
 
-    def forward(self):
-        pass
+    def forward(self, inp):
+        return self.forward1(inp)
 
     def bp(self):
         pass
 
     def img2col(self, vol):
-        return vol.flatten()
+        # img to col: (n, channel, in_nm_h, in_nm_w) => (n, channel * kernel_h * kernel_w, out_nm_h * out_nm_w)
+        X = vol.w
+        convs = self.filters
+        _, kernel_depth, kernel_w, kernel_h = convs.shape
+        l = kernel_w*kernel_h*kernel_depth
+        size1 = (vol.batch_size, kernel_w * kernel_h * self.channel, self.out_nm_w * self.out_nm_h)
+        ret = np.zeros(size1)
+        # loop through samples
+        for i in range(vol.batch_size):
+            for w0 in range(0, self.out_nm_w): # col first
+                w1 = -self.pad + w0 * self.strip
+                w2 = w1 + kernel_w
+                if w1 < 0: w1 = 0
+                if w2 < 0:
+                    continue
+                if w2 > self.in_nm_w:
+                    pass
+                for h0 in range(0, self.out_nm_h):
+                    h1 = -self.pad + h0 * self.strip
+                    h2 = h1 + kernel_h
+                    if h1 < 0: h1 = 0
+                    if h2 < 0:
+                        continue
+                    if h2 > self.in_nm_h:
+                        pass
+
+                    col = X[i, :, h1 : h2, w1 : w2,].flatten()
+                    l0 = len(col)
+                    if l0 < l:
+                        if h2 >= self.in_nm_h + self.pad or w2 >= self.in_nm_w + self.pad:
+                            col = np.append(col, [0.0]*(l-l0))
+                        else:
+                            col = np.append([0.0]*(l-l0), col)
+
+                    ret[i,:,h0 * self.out_nm_w + w0] = col[:]
+
+        return ret
+
+    @property
+    def grad(self):
+        return self.filters.grad
 
 # The convolution is intensively used for image alike input data. Hence we might have multi-dimensional data.
 # For the most use cases, three dimensional filters of size P*P*K are applied on multi dimensional input source.
@@ -35,135 +76,110 @@ class Layer(Node):
 # times > (upsampling mirror)
 class ConvLayer(Layer):
 
-    def __init__(self, inp, n, spatial_shape, convs, K, pad=0, strip=2,
-                 lr=1.0, default_conv="Gaussion", bias=None, **kw):
-        self.inp = inp # of size (n,<spatial dim>)
-        self.out = None
-        self.grad = None
-        self.n = n
-        # assert(inp, np.array)
-        self.in_nm_w, self.in_nm_h, self.channel = spatial_shape
+    def __init__(self, spatial_shape, convs, pad=0, strip=2,
+                 lr=1.0,  bias=None, **kw):
+        self.inp=None
+        self.inp_grad = np.zeros(spatial_shape)
+        self.channel, self.in_nm_w, self.in_nm_h = spatial_shape
         # Please refer to this post for details of conv filter paramter definition
         # Conv optimization: http://mp.weixin.qq.com/s/GKZeTrfTgRXmj29c_XouOQ
-        self.filters = convs
-        self.K = K
+        self.filters = convs # Vol
         self.pad = pad
         self.strip = strip
         self.LAYER_TYPE = 'conv'
         self.lr = lr
-        self.default_conv = default_conv
-        self.bias = bias or 1
 
-        cout, kernel_w, kernel_h, kernel_depth = self.filters.shape
+        K, kernel_depth, kernel_h, kernel_w = convs.shape
+        self.bias = bias # Vol
         # check dim
-        if kernel_w is not self.in_nm_h or \
-            kernel_h is not self.in_nm_w or \
-            kernel_depth is not self.channel:
-            raise Exception("inp dims do not match the conv size")
+
         self.P = None
         if kernel_w == kernel_h:
             self.p = kernel_w
-        self.out_nm_w = math.floor((self.in_nm_w + self.pad*2 - kernel_w) / self.strip + 1)
-        self.out_nm_h = math.floor((self.in_nm_h + self.pad*2 - kernel_h) / self.strip + 1)
+        self.out_nm_w = int(math.floor((self.in_nm_w + self.pad*2 - kernel_w) / self.strip + 1))
+        self.out_nm_h = int(math.floor((self.in_nm_h + self.pad*2 - kernel_h) / self.strip + 1))
 
+        self.out = None
         # TO DO:
         # Initialize filters
 
-    def uid(self, argc, key, dims):
-        ret = 1.0
-        i = 0
-        factor = 1
-
-        while i < argc:
-            factor *= dims[i]
-            i += 1
-
-        i = 0
-        while i < argc:
-            ret += (key[i]) * factor
-            factor /= dims[argc-1-i]
-            i += 1
-
-        return ret
-
-    def diff(self, inp, W_col, filters):
-        pass
-
-    def convolute1(self, inp, W_col, filters):
-        cout, kernel_w, kernel_h, kernel_depth = filters.shape
-        size1 = (self.n, self.out_nm_w*self.out_nm_h)
-        ret = np.zeros(size1)
-        for i, X in enumerate(inp):
-
-            j = 0
-            for w in range(-self.pad, kernel_w+self.pad, self.strip):
-                for h in range(-self.pad, kernel_h+self.pad, self.strip): # column first
-
-                    # conv
-                    start, end = 0, -1
-                    w0, w1, w2 = w, w + self.strip, 0
-                    if w0 < 0:
-                        w0 = 0
-                        if w1 < 0:
-                            continue
-                        w2 = -w
-                    h0, h1, h2 = h, h + self.strip, 0
-                    if h0 < 0:
-                        h0 = 0
-                        if h1 < 0:
-                            continue
-                        h2 = -h
-
-                    delta = self.uid(3, (w2, h2, 0), (kernel_w, kernel_h, kernel_depth))
-                    start += delta
-                    end -= delta
-                    # fft might be used here
-                    ret[i,j] = np.dot(X[w0:w1, h0:h1, :], W_col[start:end])
-                    j += 1
-
-        return ret
-
-    # using four nested For-Loop to implement raw ConvNet forward.
+    # using nested For-Loop to implement raw ConvNet forward.
     # This is not efficient and left to be improved in the near future
+    # See: https://github.com/karpathy/convnetjs/blob/master/src/convnet_layers_dotproducts.js
+    #      https://github.com/costapt/cs231n/blob/master/assignment2/cs231n/im2col.py
     def forward1(self, inp):
-        self.inp = inp
-        # assuming shape does not change
-        cout, kernel_w, kernel_h, kernel_depth = self.filters.shape
-        size1 = (self.n, self.out_nm_w*self.out_nm_h, cout)
+        self.inp = inp # Vol
+        n = inp.batch_size
+        # might need to check here
+        convs = self.filters
+        K, kernel_depth, kernel_h, kernel_w = convs.shape
+        size1 = (n, K, self.out_nm_h, self.out_nm_w)
         ret = np.zeros(size1)
-        # depth  * cout
-        for i, f in enumerate(self.convs):
-            # img2col
-            W_col = self.img2col(f) # 1 dimensional vec
-            out = self.convolute1(self.inp, W_col, self.filters)
-            ret[:,i] = out[:]
 
-        # the val of size (self.n,out_nm_w*out_nm_h,cout)
-        # col2img
-        ret.resize(self.n, self.out_nm_w, self.out_nm_h, cout)
+        # filters to row
+        W_row = convs.w.reshape(K, kernel_depth * kernel_h * kernel_w) # -1
+
+        # img to col: (n, channel, in_nm_h, in_nm_w) => (n, kernel_channel * kernel_h * kernel_w, out_nm_h * out_nm_w)
+        X_col = self.img2col(inp)
+        self.X_col = X_col
+        # loop through samples
+        for i in range(n):
+            # (K, kc*kh*kw) mul  (kc*kh*hw, oh*ow)
+            out = np.matmul(W_row, X_col[i,:]) + self.bias.w
+            # col2img
+            out.resize(K, self.out_nm_h, self.out_nm_w)
+            ret[i,:] = out[:]
         self.out = ret
         return ret
 
-
     def bp(self, top_layer):
-        # back propagation in practice
-        # return cost, grad of weights; the grad of weights will be stored in the
-        # based on chain rule and observation of conv, the partial derivatives are given
-        # another spatial flipped convolution over gradients from top node
-        size1 = (len(self.filters), self.kernel_w*self.kernel_h*self.kernel_depth) # cout * p * j * k
-        ret = np.zeros(size1)
-        # top_layer.grad: of size eq to (n, h, w, d) functions as a family of filters (h,w,d) == (out_nm_w, out_nm_h, cout)
-        # self.inp: of size eq to (self.n, self.in_nm_w, self.in_nm_h, self.channel)
-        for i, f in enumerate(top_layer.grad):
-            # img2col
-            W_col = self.img2col(f)
-            out = self.diff(self.inp, W_col, top_layer.grad)
-            ret[:,i] = out[:]
+        # bias_grad, partial derivatives of biases
 
-        # the val of size (self.n, l)
-        pass
+        # dW, partial derivatives of filters
+        # convol top_layer.grad with inp
+        filters = top_layer.grad
+        convs = self.filters
+        K0, kernel_depth, kernel_h, kernel_w = convs.shape
+        n, K1, out_nm_h, out_nm_w = filters.shape
 
+        # A naive explanation:
+        # for i in range(K1):
+        #     f = filters[:,i]
+        #     # convol with inp
+        #     # (n, oh, ow) conv (n, kd*kh*kw, oh, ow) => (K, kd, kh, kw):
+        #     for kw in range(kernel_w):
+        #         for kh in range(kernel_h):
+        #             for kd in range(kernel_depth):
+        #                 # self.filters.grad[i, kw, kh, kd] += 1/n *
+        # sum_(p,q,s){ X[kh + p*kernel_h, kw + q*kernel_w, kd] * grad[p,q,s] }
+        #                     pass
+        # Rearrange above loop:
+        for k in range(K1):
+            f = filters[:, k]
+            for kw in range(kernel_w):
+                for kh in range(kernel_h):
+                    for kd in range(kernel_depth):
+                        uid = (kd*kernel_h+kh)*kernel_w+kw
+                        self.filters.grad[k, kw, kh, kd] += np.dot(self.X_col[:,uid], f) / float(self.inp.batch_size)
 
+        # grad, partial derivatives of inp
+        # opposite to forward , inp computed in flipped direction
+        # (n, channel, in_nm_h, in_nm_w) <= (n, K , oh, ow) conv flipped(filter) dot g(inp)
+        for i in range(self.in_nm_h):
+            for j in range(self.in_nm_w):
+                for k in range(self.channel):
 
+                    # grad (n, K, oh, ow) conlv flipped(f) (K, kernel_h, kernel_w, kernel_depth )
+                    self.conlv(self.inp_grad, top_layer.grad, convs, (k,i,j))
+        # conv
+        return self.inp_grad, self.filters, self.bias
 
-
+    def conlv(self, target, grad, convs, index):
+        K, kernel_depth, kernel_h, kernel_w = convs.shape
+        k,i,j = index
+        for p in range(self.out_nm_h):
+            for q in range(self.out_nm_w):
+                for s in range(K):
+                    if i-p*self.strip-self.pad < 0 or j-q*self.strip-self.pad < 0:
+                        continue
+                    target[index] += grad[s,p,q] * convs[s,k,i-p*self.strip-self.pad,j-q*self.strip-self.pad]
---
+++ Correlation Metrics/ConvNet/costImpl/ConvNet/vol.py
@@ -0,0 +1,43 @@
+__author__ = 'wangyi'
+import numpy as np
+
+class Vol:
+
+    def __init__(self, batch_size, spatial_size, init_gen=None):
+        self.batch_size = batch_size
+        self.spatial_size = spatial_size
+
+        self._init(batch_size, spatial_size, init_gen)
+
+    @property
+    def shape(self):
+        return (self.batch_size,) + self.spatial_size
+
+    def _init(self, batch_size, spatial_size, init_gen):
+        if batch_size > 1:
+            self.w = np.zeros((batch_size,)+spatial_size)
+            self.grad = np.zeros((batch_size,)+spatial_size)
+        else:
+            self.w = np.zeros(spatial_size)
+            self.grad = np.zeros(spatial_size)
+
+        if hasattr(init_gen, "__len__"):
+            self.w[:] = init_gen[:]
+
+    def uid(self, argc, argv, dims):
+        ret = 1
+        i = 0
+        factor = 1
+
+        while i < argc:
+            factor *= dims[i]
+            i += 1
+
+        i = 0
+        while i < argc:
+            ret += (argv[i]) * factor
+            factor /= dims[argc-1-i]
+            i += 1
+
+        return ret
+
